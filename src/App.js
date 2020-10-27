94bc58b5dce5a6238336d73630b5ba697de9db30
Karl Beck <karljohnbeck@gmail.com>
2020-10-27 15:44:08 -0500

    basic react carosaul in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const list = ["1", "2", "3", "4", "5"] 
+const currentNumber = 0
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>carousel </h1>
+
       </header>
+      {}
     </div>
   );
 }
