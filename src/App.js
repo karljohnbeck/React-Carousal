d14e3c609820c02edde4f6d495a6a580ed692471
Karl Beck <karljohnbeck@gmail.com>
2020-10-30 11:54:39 -0500

    build a most basic carosal with react and javaScript

--- src/App.js
+++ src/App.js
@@ -1,95 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-const list = ["1", "2", "3", "4", "5"]
-const currentNumber = 0
-
-// const carousel = (function(){
-//   var box = document.querySelector('.carouselbox');
-//   var next = box.querySelector('.next');
-//   var prev = box.querySelector('.prev');
-//   var items = box.querySelectorAll('.content li');
-//   var counter = 0;
-//   var amount = items.length;
-//   var current = items[0];
-//   box.classList.add('active');
-//   function navigate(direction) {
-//     current.classList.remove('current');
-//     counter = counter + direction;
-//     if (direction === -1 && 
-//         counter < 0) { 
-//       counter = amount - 1; 
-//     }
-//     if (direction === 1 && 
-//         !items[counter]) { 
-//       counter = 0;
-//     }
-//     current = items[counter];
-//     current.classList.add('current');
-//   }
-//   next.addEventListener('click', function(ev) {
-//     navigate(1);
-//   });
-//   prev.addEventListener('click', function(ev) {
-//     navigate(-1);
-//   });
-//   navigate(0);
-// })();
-
-let num = 0
-let direction = 0
-const add = () => {
-  direction = 1
-  if (direction === 1 && 
-            counter < 0) { 
-            num + 1; 
-        }
-}
+import Carosaul from './carosaul'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>carousel </h1>
-      </header>
-
-      <div class="carouselbox">
-  <div class="buttons">
-    <button class="prev">
-      ◀ <span onClick={minus} class="offscreen">Previous</span>
-    </button>
-    <button onClick={add} class="next">
-      <span class="offscreen">Next</span> ▶ 
-    </button>
-  </div>
-  <ol class="content">
-  <li>{num}</li>
-  </ol>
-</div>
-
-      {/* <div className="container">
-        <div className="h5">Circular carousal</div>
-        <div className="carousel">
-          <div className="slider">
-            <section>1</section>
-            <section>2</section>
-            <section>3</section>
-            <section>4</section>
-            <section>5</section>
-          </div>
-          <div className="controls">
-            <button className="next"><i className="material-icons">
-              keyboard_arrow_right
-            </i>
-            </button>
-            <button className="prev"><i className="material-icons">
-              keyboard_arrow_left
-            </i>
-            </button>
-          </div>
-        </div>
-      </div> */}
-
+    <Carosaul />
     </div>
   );
 }
--- src/carosaul.js
+++ src/carosaul.js
@@ -1,2 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Carosaul extends Component {
+    state = {
+      heading: 'Categories Component',
+      isEdit: true,
+      name: "",
+      img: [
+        'https://images.unsplash.com/photo-1574144611937-0df059b5ef3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80',
+          'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+          'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=727&q=80',
+          'https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'],
+      currentImg: 0,
+    };
+
+    // componentDidMount = () => {
+    //     console.log('woo')
+    // }
+
+    next = (event) => {
+        if (this.state.currentImg < ((this.state.img).length - 1)) {
+        this.setState({
+            currentImg: this.state.currentImg + 1
+        })
+    } else {
+        this.setState({
+            currentImg: 0
+        })
+    }
+    }
+
+    previous = (event) => {
+        if (this.state.currentImg > 0) {
+            this.setState({
+                currentImg: this.state.currentImg - 1
+            })
+        } else {
+            this.setState({
+                currentImg: ((this.state.img).length - 1)
+            })
+        }
+    }
+
+    render() {
+       return (
+        <div>
+            <h3>carousel</h3>
+            <button onClick={() => this.next()} >next</button>
+        <button onClick={() => this.previous()} >previous</button>
+        <br />
+        <img src={this.state.img[this.state.currentImg]}/>
+        
+        </div>
+       )
+    }
+
+}
+
+export default Carosaul
